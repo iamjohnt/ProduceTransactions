e2400aabe9135145ced0b75a6ca8b7fcd1e54b40
John Tran <johntrran@Johns-MacBook-Air.local>
2024-08-26 12:43:43 -0400

    create Scheduler.java, responsible for orchestrating generating a transaction, sending to kafka, and doing this on a schedule

---
+++ src/main/java/org/example/Scheduler.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+// schedules recurring job to generate transaction, and send to kafka topic
+@Component
+public class Scheduler {
+
+    @Autowired
+    TransactionGenerator generator;
+
+    @Autowired
+    TransactionKafkaSender sender;
+
+    @Scheduled(fixedRate = 5000)
+    public void schedule() {
+        this.generateAndSendTransaction();
+    }
+
+    private void generateAndSendTransaction() {
+        Transaction t = generator.generateRandomTransaction();
+        sender.send(t);
+    }
+
+}
