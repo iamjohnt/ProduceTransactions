d637b2d436e296b5cd17cdd6d39a63b2df5c830b
John Tran <johntrran@Johns-MacBook-Air.local>
2024-08-26 12:41:06 -0400

    add @Configuration tag to Config class, which is required for it to be scanned for any containing beans

--- src/main/java/org/example/Config.java
+++ src/main/java/org/example/Config.java
@@ -2,8 +2,10 @@ package org.example;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-public class Configuration {
+@Configuration
+public class Config {
 
     @Value("${app.confluent.secrets}")
     private String confluentSecret;
